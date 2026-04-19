952d96e631b9d3a77eac0a1e8118fda828a68ef0
SebastianCortesIbacache <164980067+SebastianCortesIbacache@users.noreply.github.com>
2026-04-19 00:58:55 -0400

    Add files via upload
    primer upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "presion-mental-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html"
+];
+
+// Instalar
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
